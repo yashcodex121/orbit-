528627fb764d45cde81137595c817389a05c17a4
yash <sauravk0257@gmail.com>
2026-06-01 18:13:35 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,8 @@
+from .start import register as start_register
+from .help import register as help_register
+from .download import register as download_register
+
+def load_handlers(app):
+    start_register(app)
+    help_register(app)
+    download_register(app)
